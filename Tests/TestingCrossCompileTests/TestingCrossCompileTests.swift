087cc251330273a18403eb29e21917eb61762e17
Marc Prud'hommeaux <marc@skip.tools>
2025-03-12 16:01:24 -0400

    Test with expectation failure

--- Tests/TestingCrossCompileTests/TestingCrossCompileTests.swift
+++ Tests/TestingCrossCompileTests/TestingCrossCompileTests.swift
@@ -4,5 +4,5 @@ import Testing
 @Test func example() async throws {
     // Write your test here and use APIs like `#expect(...)` to check expected conditions.
     #expect(1 + 2 == 3)
-    #expect(1 == 2)
+    #expect(1 == 2) // intentional failure
 }
